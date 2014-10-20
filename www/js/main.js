f67abe16ca3a3d05e19caaa5799211ddfe1743ab
Dave Boling <kadowki@gmail.com>
2014-10-19 21:57:02 -0500

    Added images!

--- www/js/main.js
+++ www/js/main.js
@@ -14,16 +14,25 @@
 
     var canvas,
     ctx,
-    x = 0,
-    y = 0,
+    x = 5,
+    y = 5,
     WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight,
     x2 = Math.random()*(WIDTH - 20),
     y2 = Math.random()*(HEIGHT - 20),
     dx = 5,
     dy = 5,
+    dx2 = 3,
+    dy2 = 3,
     activeGame,
-    accel;
+    accel,
+    ball = new Image(),
+    hole = new Image(),
+    course = new Image();
+
+    ball.src = '../img/assets/ball.png';
+    hole.src = '../img/assets/hole.png';
+    course.src = '../img/assets/grass.jpg';
 
     function circle(x,y,r){
       ctx.beginPath();
@@ -81,12 +90,14 @@
 
     function draw(){
       clear(); //clear canvas on each pass and reanimate like Linkin Park.
-      ctx.fillStyle = 'green';
-      rect(0,0, WIDTH, HEIGHT);
-      ctx.fillStyle = 'black';
-      circle(x2, y2, 20);
-      ctx.fillStyle = 'white';
-      circle(x, y, 10);
+      //ctx.fillStyle = 'green';
+      ctx.drawImage(course, 0, 0, WIDTH, HEIGHT);
+      //ctx.fillStyle = 'black';
+      //circle(x2, y2, 20);
+      ctx.drawImage(hole, x2, y2, 60, 60);
+      //ctx.fillStyle = 'white';
+      //circle(x, y, 10);
+      ctx.drawImage(ball, x, y, 30, 30);
 
       //bounds checking
       checkBallBounds();
@@ -94,9 +105,13 @@
       x = dx;
       y = dy;
 
+      x2 += dx2;
+      y2 += dy2;
+
       //game logic
       //check if ball has touched a zombie or the hole
       checkBallPosition();
+      checkHoleBounds();
 
     }
 
@@ -107,7 +122,7 @@
 
     function checkBallPosition(){
       //hole check
-      if(dx >= x2 - 30 && dx <= x2 + 30 && dy >= y2 -30 && dy <=  y2 + 30){
+      if(dx >= x2 - 10 && dx <= x2 + 10 && dy >= y2 -10 && dy <=  y2 + 10){
         //stop the loop
         clearInterval(activeGame);
         $scope.gameIsActive = false;
@@ -118,11 +133,20 @@
     }
 
     function checkBallBounds(){
-      if (dx > WIDTH || dx < 0){
+      if (x2 + dx > WIDTH || x2 + dx < 0){
         dx = x;
       }
       if (dy > HEIGHT ||dy < 0){
         dy = y;
+      }
+    }
+
+    function checkHoleBounds(){
+      if (x2 + dx2 > WIDTH || x2 + dx2 < 0){
+        dx2 = -dx2;
+      }
+      if (y2 + dy2 > HEIGHT || y2 + dy2 < 0){
+        dy2 = -dy2;
       }
     }
 
